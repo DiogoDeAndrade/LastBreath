27f8607ec374af85c1ba5288c56abd6156c1d898
Diogo de Andrade <diogo.g.andrade@gmail.com>
2025-02-21 19:50:59 +0000

    Added screenshots, etc

--- upload.bat
+++ upload.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Set the version variable
-set version=1.1.1
+set version=1.2.0
 
 :: Use the version variable in the command
 c:\opt\butler\butler.exe push LastBreath_v%version%.zip diogoandrade/last-breath:windows --userversion %version%
